2d07f544658e4267ac7bed4323fcf2e1e7e72035
Unknown <ptrick@udel.edu>
2019-04-06 11:23:34 -0400

    retest

--- LightStripBackend/i2c_test.py
+++ LightStripBackend/i2c_test.py
@@ -7,6 +7,9 @@ if system == 'Pi':
     import smbus
     bus = smbus.SMBus(channel)
 
-data = 65
-bus.write_block_data(ADDRESS, 2, [0].extend(i for i in range(30)))
-bus.write_block_data(ADDRESS, 2, [10].extend(i for i in range(30)))
+data = [0]
+data.extend(i for i in range(30))
+bus.write_block_data(ADDRESS, 2, data)
+data = [10]
+data.extend(i for i in range(30,60))
+bus.write_block_data(ADDRESS, 2, data)
